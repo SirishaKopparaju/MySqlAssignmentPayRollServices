d711e3e059593259b4f404e777e304770b5f4ee5
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-05-04 23:35:25 +0530

    Refactor:used all the cases

--- Sql.sql
+++ Sql.sql
@@ -80,7 +80,52 @@ create table employee_department(
   primary key(employee_id,department_id)
 );
 desc employee_department;
+select * from employee;
+select * from payroll;
+select * from department;
+select * from employee_department;
+show tables;
 
+drop table employee;
+drop table payroll;
+drop table employee_department;
+
+insert into employee(name, gender, phone_number, address,startDate)
+values ('Julekha', 'f','9898989898','Pune','2021-01-10');
+insert into employee(name, gender, phone_number, address,startDate)
+values ('Rohini', 'f','8686868686','Pune','2021-10-01');
+insert into employee(name, gender, phone_number, address,startDate)
+values ('Neeta', 'f','9898955678','MP','2021-01-04');
+insert into employee(name, gender, phone_number, address,startDate)
+values ('john', 'm','9895689865','NY','2021-04-10');
+
+insert into payroll(basic_pay, deductions, income_tax, taxable_pay, tax, net_pay,emp_ID)
+values (400000,1000,500,5000,600,350000,1);
+insert into payroll(basic_pay, deductions, income_tax, taxable_pay, tax, net_pay,emp_ID)
+values (500000,2000,500,5000,600,450000,2);
+insert into payroll(basic_pay, deductions, income_tax, taxable_pay, tax, net_pay,emp_ID)
+values (600000,3000,500,5000,600,550000,3);
+insert into payroll(basic_pay, deductions, income_tax, taxable_pay, tax, net_pay,emp_ID)
+values (700000,4000,500,5000,600,650000,4);
+
+insert into department(dept_name)
+values('Sales'),('Marketing');
+insert into department(dept_name)
+values('HR');
+select sum(basic_pay) from payroll;
+select gender, sum(basic_pay) from payroll join employee group by gender;
+select gender, avg(basic_pay) from payroll join employee group by gender ;
+select gender, max(basic_pay) from payroll join employee group by gender ;
+select gender, min(basic_pay) from payroll join employee group by gender ;
+select gender, count(*) from employee group by gender;
+
+insert into employee(name, gender, phone_number, address,startDate)
+values ('Terissa', 'f','9895689865','NY','2021-04-10');
+
+insert into payroll(basic_pay, deductions, income_tax, taxable_pay, tax, net_pay,emp_ID)
+values (300000,1000,500,5000,600,350000,5);
+
+insert into employee_department(employee_id,department_id)values(1,1);
 
 
 
